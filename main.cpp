974eea0cf57ae93d5649de7b8c8553cb8a1d7e54
Kamil Marszalek <kamilmar553@gmail.com>
2024-12-19 21:23:56 +0100

    Changed pthread to std::thread

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "monitor.h"
 #include "producer.h"
 #include "consumer.h"
-#include <pthread.h>
+#include <thread>
 #include <iostream>
 #include <unistd.h>
 
@@ -11,8 +11,7 @@ struct ThreadArgs {
     Monitor* monitor;
 };
 
-void* producer_thread(void *arg) {
-    ThreadArgs* args = (ThreadArgs*)arg;
+void producer_thread(ThreadArgs* args) {
     producer *prod = args->prod;
     Monitor *monitor = args->monitor;
     bool status = true;
@@ -26,11 +25,9 @@ void* producer_thread(void *arg) {
         status = monitor->put(prod);
         sleep(1);
     }
-    return NULL;
 }
 
-void* consumer_thread(void *arg) {
-    ThreadArgs* args = (ThreadArgs*)arg;
+void consumer_thread(ThreadArgs* args) {
     consumer *cons = args->cons;
     Monitor *monitor = args->monitor;
     bool status = true;
@@ -44,7 +41,6 @@ void* consumer_thread(void *arg) {
         status = monitor->get(cons);
         sleep(1);
     }
-    return NULL;
 }
 
 int main(int argc, char *argv[]) {
@@ -52,6 +48,7 @@ int main(int argc, char *argv[]) {
         std::cout << "Usage: " << argv[0] << " <number of producers> <number of consumers> <begin> <end> <begin> <end> <capacity> <timeout>\n";
         return 1;
     }
+    
     system("rm -f producer_*.txt consumer_*.txt warehouse.txt");
     int number_of_producers = atoi(argv[1]);
     int number_of_consumers = atoi(argv[2]);
@@ -65,28 +62,31 @@ int main(int argc, char *argv[]) {
     sleep(timeout);
     producer *producers[number_of_producers];
     consumer *consumers[number_of_consumers];
-    pthread_t producer_threads[number_of_producers];
-    pthread_t consumer_threads[number_of_consumers];
+    std::thread producer_threads[number_of_producers];
+    std::thread consumer_threads[number_of_consumers];
     ThreadArgs producer_args[number_of_producers];
     ThreadArgs consumer_args[number_of_consumers];
 
     for (int i = 0; i < number_of_producers; i++) {
         producers[i] = new producer(a, b, i);
         producer_args[i] = {producers[i], nullptr, monitor};
-        pthread_create(&producer_threads[i], NULL, producer_thread, &producer_args[i]);
+        producer_threads[i] = std::thread(producer_thread, &producer_args[i]);
     }
+
     for (int i = 0; i < number_of_consumers; i++) {
         consumers[i] = new consumer(c, d, i);
         consumer_args[i] = {nullptr, consumers[i], monitor};
-        pthread_create(&consumer_threads[i], NULL, consumer_thread, &consumer_args[i]);
+        consumer_threads[i] = std::thread(consumer_thread, &consumer_args[i]);
     }
 
     for (int i = 0; i < number_of_producers; i++) {
-        pthread_join(producer_threads[i], NULL);
+        producer_threads[i].join();
     }
+
     for (int i = 0; i < number_of_consumers; i++) {
-        pthread_join(consumer_threads[i], NULL);
+        consumer_threads[i].join();
     }
+
     delete monitor;
     return 0;
 }
